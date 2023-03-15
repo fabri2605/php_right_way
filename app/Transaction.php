87bc98f410929164fefebc375d4acb2da6820dd3
Fabri Di Paolo <fabridp77@gmail.com>
2023-03-15 09:30:37 -0300

    new_user

--- app/Transaction.php
+++ app/Transaction.php
@@ -1,19 +1,37 @@
 <?php
 declare(strict_types=1);
 
-namespace App\PaymentGateway\Paddle\Transaction;
+use Ramsey\Uuid\UuidFactory;
 
 class Transaction
 {
-    public $STATUS_PAID;
+    public $id;
+    private $status;
+    public const STATUS_PAID = 'paid';
     public const STATUS_PENDING = 'pending';
     public const STATUS_DECLINED = 'declined';
 
-    public function __construct($STATUS_PAID)
+    private const ALL_STATUS = [
+        self::STATUS_PAID => 'Paid',
+        self::STATUS_PENDING => 'Pending',
+        self::STATUS_DECLINED => 'Declined',
+    ];
+
+    public function __construct()
     {
-        echo 'hi';
-        $this->STATUS_PAID = $STATUS_PAID;
+        $idNow = new UuidFactory();
+        $this->status = self::STATUS_PENDING;
+        $this->id = $idNow->uuid4();
+    }
+
+    public function setStatus(string $stat)
+    {
+        if (!isset(self::ALL_STATUS[$stat])) {
+            ini_set('display_errors', 1);
+
+            echo 'bad';
+            throw new Exception('Bad parameters!');
+        }
+        $this->status = $stat;
     }
 }
-
-?>
--- public/index.php
+++ public/index.php
@@ -1,22 +1,23 @@
 <?php
 
-require __DIR__ . '/../vendor/autoload.php';
 
-// declare(strict_types=1);
-/* declare(strict_types=1);
+declare(strict_types=1);
+
+require __DIR__ . '/../vendor/autoload.php';
+require '../app/Transaction.php';
+//new comment
 // /var/www/html/learnphptherightway-project/ 
 $root = dirname(__DIR__) . DIRECTORY_SEPARATOR;
 define('APP_PATH', $root . 'app' . DIRECTORY_SEPARATOR);
 define('FILES_PATH', $root . 'transaction_files' . DIRECTORY_SEPARATOR);
 define('VIEWS_PATH', $root . 'views' . DIRECTORY_SEPARATOR);
-require(APP_PATH . 'fileViewer.php');
+
+/* require(APP_PATH . 'fileViewer.php');
 $csvArray = csvsToArray(FILES_PATH);
 $totals = getTotal($csvArray); */
 
-$trans = new Transaction('PAID');
-
-$id = new Ramsey\Uuid\UuidFactory();
-
-echo $trans->STATUS_PAID;
-
-/* include_once('../views/transactions.php'); */
+$trans = new Transaction();
+$trans->setStatus('pending');
+echo '<pre>';
+var_dump($trans);
+echo '<pre>';
